2b5fb9c11d4ae09cb62c90de80368ee2e137409c
Bishal Raj Shrestha <shresth39@uni.coventry.ac.uk>
2021-06-11 14:48:47 +0545

    Home Page Template
    layout and homepage

--- skilldrill/settings.py
+++ skilldrill/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_DIR = os.path.join(BASE_DIR, "skilldrill/static")
+STATICFILES_DIRS = [STATIC_DIR]
