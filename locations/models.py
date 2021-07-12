5f706466775407d494662fff9e2df36a69dceea0
Bishal Raj Shrestha <shresth39@uni.coventry.ac.uk>
2021-07-12 10:32:32 +0545

    Category_services_x

--- locations/models.py
+++ locations/models.py
@@ -22,16 +22,23 @@ class Location(models.Model):
     def __str__(self):
         return f"{self.id}. {self.location}"
 
+class Category(models.Model):
+    category = models.CharField(max_length=200)
+    manager = models.CharField(max_length=100)
+    # location = models.ManyToManyField(Location, blank=True, related_name="categories")
+
+    def __str__(self):
+        return self.category
 
 class Services(models.Model):
-    category = models.CharField(choices=CATEGORY_CHOICES, null=True, max_length=30)
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE, related_name="rel_cat")
     title = models.CharField(max_length=200)
     charge = models.IntegerField()
     time = models.IntegerField(null=True)
     description = models.CharField(max_length=2000, null=True)
-    manager = models.CharField(max_length=100)
-    location = models.ManyToManyField(
-        Location, blank=True, related_name="services")
+    professional = models.CharField(max_length=100) #Todo: Many to Many
+    location = models.ManyToManyField(Location, blank=True, related_name="services")
+
 
     def __str__(self):
         return f"{self.title}: {self.charge}"
--- locations/views.py
+++ locations/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from .models import Location, Services
+from .models import Location, Services, Category
 
 # Create your views here.
 
 def location(request, location_name): #, service_id
     locations = Location.objects.get(location=location_name)
-    # services = Services.objects.all() #get(id=service_id)
+    category = Category.objects.all()
     return render(request, "locations/location.html",{
-        "locations":locations, "services":locations.services.all()
+        "locations":locations, "services":locations.services.all(), "category":category
     })
 
+def category(request, category_name):
+    category = Category.objects.get(title=category_name)
+    return render(request, "locations/service.html", {
+        "category":category
+    })
 
 def service(request, service_name, location_name):
     services = Services.objects.get(title=service_name)
@@ -17,4 +22,3 @@ def service(request, service_name, location_name):
     return render(request, "locations/service.html",{
         "services":services, "locations":locations
     })
-# Create your views here.
