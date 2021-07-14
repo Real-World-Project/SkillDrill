9f38094d1acfd3124dfc990e4ed147e1bf960684
banishsapkota <banish.sapkota123@gmail.com>
2021-07-14 08:34:59 +0545

    adminpanel
    admin panel 1st

--- skilldrill/settings.py
+++ skilldrill/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'blog',
     'accounts',
+    'adminpanel',
 
     # authentication app
     'django.contrib.sites',
--- skilldrill/urls.py
+++ skilldrill/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     # path('accounts/', include('django.contrib.auth.urls')),
     path('logout', LogoutView.as_view(), name='logout'),
     path('accounts/login/', LoginView.as_view(), name='login'),
+    path('dashboard/', include('adminpanel.urls'))
 ]
